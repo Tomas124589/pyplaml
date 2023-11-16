2a1eee0b20580d99dad59f80d297dd4852ecd66e
Tomáš Orviský <orvisky.tomas@centrum.cz>
2023-11-16 16:04:49 +0100

    Added examples

--- tests/ParserTest.py
+++ tests/ParserTest.py
@@ -2,40 +2,40 @@ import unittest
 from pyPlantUML import *
 
 
-class pyPlantUMLTest(unittest.TestCase):
+class PyPlantUMLTest(unittest.TestCase):
 
     def test_input_elements(self):
         p = PUMLParser()
-        d = p.parseFile("inputs/01_elements.puml")
+        d = p.parseFile("../examples/01_elements.puml")
 
         self.assertIsNotNone(d)
 
     def test_class_tree(self):
         p = PUMLParser()
-        d = p.parseFile("inputs/02_class_tree.puml")
+        d = p.parseFile("../examples/02_class_tree.puml")
 
         self.assertIsNotNone(d)
 
     def test_relations(self):
         p = PUMLParser()
-        d = p.parseFile("inputs/03_relations.puml")
+        d = p.parseFile("../examples/03_relations.puml")
 
         self.assertIsNotNone(d)
 
     def test_relations_extra(self):
         p = PUMLParser()
-        d = p.parseFile("inputs/04_relations_extra.puml")
+        d = p.parseFile("../examples/04_relations_extra.puml")
 
         self.assertIsNotNone(d)
 
     def test_class_attributes(self):
         p = PUMLParser()
-        d = p.parseFile("inputs/05_class_attributes.puml")
+        d = p.parseFile("../examples/05_class_attributes.puml")
 
         self.assertIsNotNone(d)
 
     def test_class_tree_with_attributes(self):
         p = PUMLParser()
-        d = p.parseFile("inputs/06_class_tree_with_attributes.puml")
+        d = p.parseFile("../examples/06_class_tree_with_attributes.puml")
 
         self.assertIsNotNone(d)
