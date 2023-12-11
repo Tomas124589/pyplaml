c8d6a5621835807bf2260af96aefbdb280a956e1
Tomáš Orviský <orvisky.tomas@centrum.cz>
2023-12-11 19:37:11 +0100

    Added scaling params to utility

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ from pyplaml import *
 import argparse
 
 from pyplaml.layout.spring_layout import SpringLayout
+from pyplaml.layout.kamada_kawai import KamadaKawai
+from pyplaml.layout.dot_layout import DotLayout
 from pyplaml.layout.hierarchical_layout import HierarchicalLayout
 
 
 class MainScene(MovingCameraScene):
     file: str
     animate: bool
+    scale_x: float
+    scale_y: float
 
     def construct(self):
         parser = PUMLParser()
         d: Diagram = parser.parse_file(self.file)
 
-        layout = HierarchicalLayout(d)
+        layout = SpringLayout(d)
         layout.apply()
-        layout.scale(2, 2.5)
+        layout.scale(self.scale_x, self.scale_y)
 
         self.camera.background_color = WHITE
         Text.set_default(font_size=16)
@@ -28,12 +32,6 @@ class MainScene(MovingCameraScene):
         d.animate = self.animate
 
         d.draw()
-
-    def set_file(self, file: str):
-        self.file = file
-
-    def set_animate(self, animate: bool):
-        self.animate = animate
 
 
 if __name__ == "__main__":
@@ -45,12 +43,16 @@ if __name__ == "__main__":
 
     argparser.add_argument("file")
     argparser.add_argument("-a", "--animate", action="store_true")
+    argparser.add_argument("-sx", "--scale-x", default=1, type=float)
+    argparser.add_argument("-sy", "--scale-y", default=1, type=float)
 
     args = argparser.parse_args()
 
     scene = MainScene()
 
-    scene.set_file(args.file)
-    scene.set_animate(args.animate)
+    scene.file = args.file
+    scene.animate = args.animate
+    scene.scale_x = args.scale_x
+    scene.scale_y = args.scale_y
 
     scene.render()
