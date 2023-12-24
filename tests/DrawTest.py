5a36e525d5136c60b48c20389edca77c5ece6a78
Tomáš Orviský <orvisky.tomas@centrum.cz>
2023-12-24 14:55:22 +0100

    Sorted draw test, added failure message

--- tests/DrawTest.py
+++ tests/DrawTest.py
@@ -1,12 +1,12 @@
 import unittest
 
-import os
+import os, sys
 from glob import glob
 
 
 class DrawTest(unittest.TestCase):
     def test_all_examples_draw(self):
-        examples = glob("../examples/*.puml")
+        examples = sorted(glob("../examples/*.puml"))
 
         for e in examples:
-            self.assertEqual(os.system("python ../main.py " + e + " -sx 3 -sy 3"), 0)
+            self.assertEqual(os.system("python ../main.py " + e + " -sx 3 -sy 3"), 0, 'Drawing "' + e + '" failed')
