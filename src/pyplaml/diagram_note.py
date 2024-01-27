8318697dbf5578192cbf162af844d6f3bbb3a4a5
Tomáš Orviský <orvisky.tomas@centrum.cz>
2024-01-27 13:36:18 +0100

    DiagramEdge redraw super call

--- src/pyplaml/diagram_note.py
+++ src/pyplaml/diagram_note.py
@@ -14,6 +14,7 @@ class DiagramNote(DiagramObject):
         self.redraw()
 
     def redraw(self):
+        super().redraw()
         rect = Rectangle(fill_color="#feffdd", fill_opacity=1, stroke_color="#8c8c83", stroke_width=1)
         text = Text(self.text, color=BLACK).scale(0.75)
         rect.stretch_to_fit_width(text.width + 0.1)
