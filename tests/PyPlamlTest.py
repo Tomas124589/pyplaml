0822d77157a422b47e426bdc6cc0690902f0b149
Tomáš Orviský <orvisky.tomas@centrum.cz>
2024-01-27 12:13:49 +0100

    Unit tests refactor

--- tests/PyPlamlTest.py
+++ tests/PyPlamlTest.py
@@ -1,33 +1,31 @@
-import unittest
-
 import os
+import unittest
 from glob import glob
+
 from pyplaml import *
 
 
 # noinspection PyBroadException
 class PyPlamlTest(unittest.TestCase):
-    def test_all_examples_lex(self):
-        examples = sorted(glob("../examples/plantuml/*.puml"))
-        l = PUMLexer()
+    def setUp(self):
+        self.parser = PUMLParser()
+        self.examples_sorted = sorted(glob("../examples/plantuml/*.puml"))
 
-        for e in examples:
-            l.testFile(e, False)
+    def test_lexer(self):
+        for example_file in self.examples_sorted:
+            self.parser.lexer.testFile(str(example_file), False)
 
-        self.assertTrue(True)
-
-    def test_all_examples_parse(self):
-        examples = sorted(glob("../examples/plantuml/*.puml"))
-        l = PUMLParser()
-
-        for e in examples:
+    def test_parser(self):
+        for example_file in self.examples_sorted:
             try:
-                self.assertIsNotNone(l.parse_file(e), "Parser error in {}".format(e))
+                self.assertIsNotNone(self.parser.parse_file(example_file), f"Parser error in {example_file}")
             except:
-                self.assertTrue(False, "Parser exception in {}".format(e))
+                self.assertTrue(False, f"Parser exception in {example_file}")
 
-    def test_all_examples_draw(self):
-        examples = sorted(glob("../examples/plantuml/*.puml"))
-
-        for e in examples:
-            self.assertEqual(os.system("python ../main.py " + e), 0, "Drawing \"{}\" failed".format(e))
+    def test_drawing(self):
+        for e in self.examples_sorted:
+            self.assertEqual(
+                os.system("python ../main.py " + e),
+                0,
+                f"Drawing \"{e}\" failed"
+            )
